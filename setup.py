dbe24f70b945e231c17da48ca80f34622d6e6104
naoiwata <orrrizzle@gmail.com>
2014-01-20 14:45:09 +0900

    Change border line’s color gray

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 import os, sys
 
-version = '0.2.17'
+version = '0.2.18'
 long_description = '\n'.join([
         open(os.path.join("src", "README.txt")).read(),
         open(os.path.join("src", "AUTHORS.txt")).read(),
